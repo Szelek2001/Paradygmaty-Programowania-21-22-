aeb3ee01906f8ce7f865675f50600904c19ea402
Rafał <rafalkruszyna10@gmail.com>
2021-10-09 17:34:28 +0200

    2 funkcje z testami

--- Lab_1/lista_1.ml
+++ Lab_1/lista_1.ml
@@ -6,5 +6,17 @@ let rec flatten1 xss =
      else List.hd xss @ flatten1(List.tl xss);;
 
 flatten1 [[1;2]; [1;2]; [63;74]] = [1;2;1;2;63;74];;
- flatten1 [["JANEK"; "KOT"]; ["MILOSZ"]] = ["JANEK";"KOT";"MILOSZ"];;
+flatten1 [["JANEK"; "KOT"]; ["MILOSZ"]] = ["JANEK";"KOT";"MILOSZ"];;
 flatten1 [] = [];;
+
+
+(*Zadanie 2*)
+
+let rec count(x,xs) =
+    if List.length  xs = 0 then 0
+    else if x=List.hd xs then 1 + count(x,List.tl xs)
+    else 0 + count(x,List.tl xs);;
+
+count ("x",["x";"x";"x";"x"])=4;;
+count (2,[3;2;1;1])=1;;
+count ("y",["2";"3";"4";"fd"])=0;;
