a84a916c9f312ab258491157b46f925a9b4dfaca
Rafał <rafalkruszyna10@gmail.com>
2021-10-17 15:37:03 +0200

    Add 2

--- Lab_2/lista_2.ml
+++ Lab_2/lista_2.ml
@@ -1,1 +1,20 @@
-2+2;;
+(*Rafał Kruszyna*)
+
+(*Zadanie 2*)
+let rec fib n =
+    match n with
+        0 -> 0
+       |1 -> 1
+       | _ -> fib(n-2) + fib(n-1);;
+
+let fibTail n =
+    let rec fibIn(n,f1,f2) =
+      match n with
+        0 -> f1
+       |1 -> f2
+       | _ -> fibIn(n-1, f2, f1+f2) in
+       fibIn(n, 0, 1);;
+
+(*Zadanie 4*)
+
+let [_; _; x; _; _;] =  [2; 3; 4; 5; 1;];;
