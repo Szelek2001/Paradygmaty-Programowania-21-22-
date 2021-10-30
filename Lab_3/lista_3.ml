5e710236a8b6cc693026c938a118816922db7c9f
Rafał <rafalkruszyna10@gmail.com>
2021-10-30 12:59:59 +0200

    Ocaml zad 2 i 3

--- Lab_3/lista_3.ml
+++ Lab_3/lista_3.ml
@@ -8,5 +8,10 @@ let curry3_zl = function f -> function x -> function y -> function z -> f(x, y, 
 
 let sumProd xs =
     List.fold_left (fun (s, p) h -> (s + h, p * h)) (0, 1) xs;;
-sumProd [2;3;4];;
+
+sumProd[2; 3; 4; 5] = (14, 120);;
+sumProd[-2; 7; 0] = (5, 0);;
+sumProd[-5; 3; -4; -9; 10] = (-5, -5400);;
+
+
 
